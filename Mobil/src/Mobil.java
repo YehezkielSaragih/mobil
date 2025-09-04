d18bdb6e38cd820e8a5c8523fddf656e48a65aa9
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-04 14:40:28 +0700

    add parent

---
+++ Mobil/src/Mobil.java
@@ -0,0 +1,13 @@
+public class Mobil {
+    // Attribute
+    private String policeNumber;
+    private int tahun;
+
+    // Constructor
+    public Mobil(String policeNumber, int tahun){
+        this.policeNumber = policeNumber;
+        this.tahun = tahun;
+    }
+
+
+}
