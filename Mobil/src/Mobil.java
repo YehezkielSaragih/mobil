9b2925fecedb0f65eaf36c0ed4181cf36bc7b284
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-04 14:49:17 +0700

    add parent attribute constructor and method

--- Mobil/src/Mobil.java
+++ Mobil/src/Mobil.java
@@ -1,13 +1,31 @@
 public class Mobil {
+
     // Attribute
     private String policeNumber;
-    private int tahun;
+    private int year;
 
     // Constructor
     public Mobil(String policeNumber, int tahun){
         this.policeNumber = policeNumber;
-        this.tahun = tahun;
+        this.year = tahun;
     }
 
+    // Set Get
+    public void setPoliceNumber(String nama) {
+        this.policeNumber = policeNumber;
+    }
+    public String getPoliceNumber() {
+        return policeNumber;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public int getYear() {
+        return year;
+    }
 
+    // Method
+    public void printInfo() {
+        System.out.println("Info Mobil");
+    }
 }
